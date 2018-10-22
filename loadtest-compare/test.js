f7bda587d7f485d08df2467bf513ba3ab6e2e206
Harry Sadler <harrypatricksadler@gmail.com>
2018-10-22 12:41:09 -0700

    did some load testing using minikube and a single docker instance

--- loadtest-compare/test.js
+++ loadtest-compare/test.js
@@ -3,42 +3,49 @@
 
 const axios = require('axios');
 
-const reqUrl = 'http://192.168.99.100:32294/';
-const reqAmount = 10;
+// const reqUrl = 'http://192.168.99.100:32118/'; // Kubes
+const reqUrl = 'http://localhost:8080'; // bare Docker instance
+const reqAmounts = [
+	1,
+	10,
+	100,
+	1000,
+	5000
+];
 
-// ex:
-// axios.all([
-// 		axios.get(reqUrl),
-// 		axios.get(reqUrl),
-// 	]).then(axios.spread((
-// 		response1,
-// 		response2
-// 	) => {
-// 		console.log('res 1: ', response1.data);
-// 		console.log('res 2: ', response2.data);
-// 	})).catch(error => {
-// 		console.log(error);
-// 	});
+var getNow = function() {
+	var hrTime = process.hrtime();
+	return hrTime[0] * 1000000 + hrTime[1] / 1000;
+};
 
-var axRequests = [];
-for (var i = 0; i < 10; i++) {
-	axRequests.push(axios.get(reqUrl));
-}
-axios.all(axRequests)
-	.then(results => {
-		results.forEach(r => {
-			console.log(r.data);
+var performRequestsRecursively = function(pos) {
+	var reqAmount = reqAmounts[pos];
+	var startTime = getNow();
+	var axRequests = [];
+	for (var i = 0; i < reqAmount; i++) {
+		axRequests.push(axios.get(reqUrl));
+	}
+	axios.all(axRequests)
+		.then(results => {
+			results.forEach(r => {
+				if(r.data !== 'res success...') {
+					console.log(r.data);
+				}
+			});
+			var endTime = getNow();
+			var latency = (endTime - startTime) / 1000000;
+			console.log('latency for request amount: ' + reqAmount + ' = ' + latency);
+			if(pos < reqAmounts.length - 1) {
+				// give time to recover
+				setTimeout(function () {
+					performRequestsRecursively(pos + 1);
+				}, 1000);
+			}
+		})
+		.catch(errors => {
+			console.log('Errors: ', errors.code);
 		});
-	});
+};
 
-
-
-
-
-
-
-
-
-
-
+performRequestsRecursively(0);
 
